6c49e31ca9d8098efae46c4e7bff56dd91bb57cd
Jennifer Shi <shiyating1998@gmail.com>
2024-12-17 12:01:45 -0500

    704-binary search

--- Array/704-binary-search.py
+++ Array/704-binary-search.py
@@ -1,61 +1,84 @@
-# https://leetcode.cn/problems/binary-search/description/
-'''
-给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+# https://leetcode.com/problems/binary-search/description/
 
-示例 1:
+# Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 
-输入: nums = [-1,0,3,5,9,12], target = 9
-输出: 4
-解释: 9 出现在 nums 中并且下标为 4
-示例 2:
-
-输入: nums = [-1,0,3,5,9,12], target = 2
-输出: -1
-解释: 2 不存在 nums 中因此返回 -1
+# You must write an algorithm with O(log n) runtime complexity.
 
 
-提示：
+# Example 1:
 
-你可以假设 nums 中的所有元素是不重复的。
-n 将在 [1, 10000]之间。
-nums 的每个元素都将在 [-9999, 9999]之间。
-'''
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+# Explanation: 9 exists in nums and its index is 4
+# Example 2:
+
+# Input: nums = [-1,0,3,5,9,12], target = 2
+# Output: -1
+# Explanation: 2 does not exist in nums so return -1
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 104
+# -104 < nums[i], target < 104
+# All the integers in nums are unique.
+# nums is sorted in ascending order.
+
+
+# Author: Jennifer Shi
+# Date: 12/17/2024
+# Analysis:
+# Input: (int) Sorted array - unique value,  (int)target value
+# Expected output: (int) index of target value or -1 if not found
+# Solution 1: Brute force, O(n)
+# Solution 2: Binary search, O(logn)
+# Time complexity: O(logn)
+# Space complexity: O(1), no additional space needed    
+# Key Takeaways: 
+    # The loop invariant is that the interval is [low, high] in our case, inclusive on both ends
+    # if the loop invariant is [low, high), not inclusive on high, then the condition and variable update is different
+      
+
 from typing import List
-
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1
-        while (left <= right):
-            mid = (left + right ) // 2
+        low = 0 
+        high = len(nums) - 1
+        while (low <= high):
+            mid = (low+high)//2
             if (nums[mid] == target):
-                return mid
+                return mid 
             elif (nums[mid] > target):
-                right = mid - 1
+                high = mid - 1
             else:
-                left = mid + 1
+                low = mid + 1
+        return -1 
 
-        return -1
 
+# Test case 1
+# Expected output: 4
 nums = [-1,0,3,5,9,12]
 target = 9
+t1 = Solution()
+assert t1.search(nums,target) == 4
 
-nums = []
-target = 2
+# Test case 2
+# Expected output: -1
+nums = [-1,0,3,5,9,12]
+target = 2  
+t2 = Solution()
+assert t2.search(nums,target) == -1
 
-nums = [2]
-target = 2
+# Test case 3 - array length of 1, not found
+# Expected output: -1
+nums = [1]
+target = 2 
+t3 = Solution()
+assert t3.search(nums,target) == -1
 
-nums = [2,4,6]
-target = 2
-
-s = Solution()
-print(s.search(nums,target))
-
-# Time complexity: O(logn)
-# Space complexity: O(1), no additional space needed
-# 一次过，不错
-# 2024/3/30
-
-#重点是定义区间
+# Test case 4 - array leng of 1, found 
+nums = [1]
+target = 1
+t4 = Solution()
+assert t4.search(nums,target) == 0 
---
+++ Array/array-fundamentals.md
@@ -0,0 +1,8 @@
+## Array
+
+Array is a data structure that holds a collection of items of the same type. Stored in consecutive memory locations.
+
+O(1) access, insertion at the end 
+O(n) search, insertion, deletion
+
+
