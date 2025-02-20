14b79ac7d3f8c9b5fbaeee6e2794bf3f12a4ad29
Jennifer Shi <shiyating1998@gmail.com>
2025-02-19 23:42:06 -0500

    wed

---
+++ Dynamic Programming/139-word-break.py
@@ -0,0 +1,12 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+
+        dp[0] = True 
+
+        for i in range(0, len(s) + 1):
+            for word in wordDict:
+                if len(word) <= i :
+                    dp[i] = dp[i] or dp[i-len(word)] and s[i - len(word):i] == word
+
+        return dp[-1]
